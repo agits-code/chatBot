d1da6b44d1953f2404f2667a4acd58a2a0ba070e
agits-code <nrrqpdMauv27012020>
2024-02-09 16:14:36 +0100

    prova 8

--- netlify/functions/fetchApiKey/fetchApiKey.js
+++ netlify/functions/fetchApiKey/fetchApiKey.js
@@ -1,10 +1,28 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+exports.handler = async function (event, context) {
+  if (event.httpMethod == "OPTIONS") {
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "GET, POST, OPTION",
+      },
+    };
+  }
 const handler = async (event) => {
   try {
     const subject = event.queryStringParameters.name || 'World'
+    
+  
     return {
       statusCode: 200,
       body: JSON.stringify({ message: `Hello ${subject}` }),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "GET, POST, OPTION",
+      },
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
