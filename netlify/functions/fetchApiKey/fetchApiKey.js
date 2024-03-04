6115405c213b6d87b7f00bb811848a30babcbe2c
agits-code <nrrqpdMauv27012020>
2024-03-04 19:05:34 +0100

    body: JSON.stringify({ reply: response.choices[0].message }),

--- netlify/functions/fetchApiKey/fetchApiKey.js
+++ netlify/functions/fetchApiKey/fetchApiKey.js
@@ -9,13 +9,23 @@ const handler = async (event) => {
   try {
 
    console.log(event.body)
-
+   // {"messages":[{"role":"system","content":"You are a helpful assistant."},{"content":"hello","role":"user"},{"content":"spiegami pi","role":"user"},{"content":"in inglese","role":"user"},{"content":"e anche in tedesco","role":"user"},{"content":"quale è capitale di cile?","role":"user"}]}
     //const subject = event.queryStringParameters.name || 'World'
    // Assicurati che il body della richiesta sia parsato correttamente
    const body = JSON.parse(event.body);
    // Adatta questa linea in base alla struttura dei tuoi dati
    const messages = body.messages; 
    console.log(messages)
+   /*
+   [
+  { role: 'system', content: 'You are a helpful assistant.' },
+  { content: 'hello', role: 'user' },
+  { content: 'spiegami pi', role: 'user' },
+  { content: 'in inglese', role: 'user' },
+  { content: 'e anche in tedesco', role: 'user' },
+  { content: 'quale è capitale di cile?', role: 'user' }
+]
+   */
    const response = await openai.chat.completions.create({
      model: 'gpt-4',
      messages: messages,
@@ -24,12 +34,30 @@ const handler = async (event) => {
    });
     
   console.log(response)
+  /*
+  {
+  id: 'chatcmpl-8ysUYkv2CnbUT7THJ791AMIx2FxQ9',
+  object: 'chat.completion',
+  created: 1709519838,
+  model: 'gpt-4-0613',
+  choices: [
+    {
+      index: 0,
+      message: [Object],
+      logprobs: null,
+      finish_reason: 'stop'
+    }
+  ],
+  usage: { prompt_tokens: 56, completion_tokens: 9, total_tokens: 65 },
+  system_fingerprint: null
+}
+  */
   const messageContent = response.choices[0].message.content;
-  console.log(messageContent);
+  console.log(messageContent); // La capitale del Cile è Santiago.
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ reply: response.data }),
+      body: JSON.stringify({ reply: response.choices[0].message }),
       
     }
   } catch (error) {
